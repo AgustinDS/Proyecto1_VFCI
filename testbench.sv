a26194ed6520a79d9df792b20f12189de33a221c
AgustinDS <ahusjads@gmail.com>
2020-11-08 16:12:02 -0600

    Update testbench.sv

---
+++ testbench.sv
@@ -0,0 +1,13 @@
+`timescale 1ns/1ps
+`include "fifo.sv"
+`include "interface_transactions.sv"
+`include "driver.sv"
+`include "checker.sv"
+`include "score_board.sv"
+`include "agent.sv"
+`include "ambiente.sv"
+`include "test.sv"
+
+///////////////////////////////////
+// Módulo para correr la prueba  //
+///////////////////////////////////

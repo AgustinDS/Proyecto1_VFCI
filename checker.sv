67edd5b7b2a74c7e17bab8df22546e6e5c4476a2
AgustinDS <ahusjads@gmail.com>
2020-11-08 15:08:42 -0600

    Update Checker

--- checker.sv
+++ checker.sv
@@ -12,7 +12,6 @@ class checker #(parameter drvrs = 4,parameter drvr_bit=2, parameter pckg_sz = 32
 	trans_sb_mbx chckr_sb_mbx;	// Mailbox checker-scoreboard
   	trans_bus chckr_mntr_mbx;	// Mailbox checker-monitor
   	trans_bus chckr_agnt_mbx;	// Mailbox para recibir las transacciones enviadas al driver, se debe conectar al mbx agente-driver
-  	// trans_bus auxiliar;		// Item auxiliar para emular el fifo de cada dispositivo
 
   	trans_bus #(.drvrs(drvrs), .drvr_bit(drvr_bit), .pckg_sz(pckg_sz), .broadcast(broadcast)) from_mntr; 		//Mensaje para comunicación con el monitor
   	trans_bus #(.drvrs(drvrs), .drvr_bit(drvr_bit), .pckg_sz(pckg_sz), .broadcast(broadcast)) from_agnt;
@@ -31,27 +30,55 @@ class checker #(parameter drvrs = 4,parameter drvr_bit=2, parameter pckg_sz = 32
 			// sb_item = new();
 			sb_item.clean();
 			chckr_agnt_mbx.get(from_agnt);
-			trans_item.print("Checker: Transacción recibida desde el agente.");
-			case(trans_item.tipo)
-				trans: begin
-					// Esperar delay / pos o neg edge
-					// Obtener D_push y push del monitor para dispistivo destino
-					// Ver que los otros dispositivos tengan push en 0
-					// Comparar dato del monitor con el del agente
-					// Enviar resultado al sb
-				end
+			from_agnt.print("Checker: Transacción recibida desde el agente.");
+			chckr_mntr_mbx.get(from_mntr);
+			from_mntr.print("Checker: Transacción recibida desde el monitor.");
+			if(from_mntr.tipo == from_agnt.tipo) begin
+				case(trans_item.tipo)
+					trans: begin
+						if(from_mntr.dato == from_agnt.dato) begin
+							$display("Checker: Transacción completada con éxito.");
+							//sb_item.dato_transmitido
+							//sb_item.Origen
+							//sb_item.Destino
+							//sb_item.tiempo_envio
+							//sb_item.tiempo_recibido
+							
+							// Enviar al scoreboard
+						end else
+						begin
+							$display("Checker: [ERROR] Dato incorrecto recibido en el dispositivo.");
+							// Enviar al scoreboard
+						end
+					end
 				
-				reset: begin
-					// Esperar delay/pos o neg edge
-					// Recibir info del mntr y ver que todo sea 0!!!
-				end
-				
-				broadcast: begin
-					// Esperar delay / pos o neg edge
-					// Obtener D_push y push del monitor para todos los dispositivos excepto origen
-					// Comparar dato del monitor con el del agente y verificar señal de push
-					// Enviar resultado al sb
-				end
+					reset: begin
+						if(from_mntr.dato == {16{1'b0}}) begin
+							$display("Checker: Reset completado con éxito.");
+							// Enviar al scoreboard
+						end else
+						begin
+							$display("Checker: [ERROR] Reset no se realizó correctamente.");
+							// Enviar al scoreboard
+						end
+					end
+					
+					broadcast: begin
+						if(from_mntr.dato == from_agnt.dato) begin
+							$display("Checker: Broadcast completado con éxito.");
+							// Enviar al scoreboard
+						end else
+						begin
+							$display("Checker: [ERROR] Dato incorrecto recibido en el dispositivo.");
+							// Enviar al scoreboard
+						end
+					end
+				endcase
+			end else
+			begin
+				$display("Checker: [ERROR] El tipo de transacción recibida del monitor no coincide con la del agente.");
+				// Enviar al scoreboard
+			end
 		end
 	endtask
 endclass

f67356ce6188cdd887bfb212b28bb22a7bd4d632
AgustinDS <ahusjads@gmail.com>
2020-11-08 19:50:18 -0600

    correccion sb

--- scoreboard.sv
+++ scoreboard.sv
@@ -8,7 +8,7 @@
 // Proposito General:
 // Scoreboard del testbench del Bus Serial.
 
-class scoreboard #(parameter drvrs = 4,parameter drvr_bit=2, parameter pckg_sz = 16, parameter broadcast = {8{1'b1}});
+class scoreBoard #(parameter drvrs = 4,parameter drvr_bit=2, parameter pckg_sz = 16, parameter broadcast = {8{1'b1}});
 	trans_sb_mbx sb_chckr_mbx;	// Mailbox checker-scoreboard
   	trans_sb_mbx sb_agnt_mbx;	// Mailbox agente-sb
   	
